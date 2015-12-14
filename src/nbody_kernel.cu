155d75451346d99de3a298252d27f1bd023924ab
Herbstrith <viniherbstrith@gmail.com>
2015-12-14 15:48:10 -0200

    making use of dinamyc parallelism

--- src/nbody_kernel.cu
+++ src/nbody_kernel.cu
@@ -4,12 +4,63 @@
 
 // calculate_forces kernel function ... here is where we start
 
+__global__ void calculate_forces( int nbodies, float4 *bodies)
+{
+  triangle <<< 1, 1 >>> (0, nbodies, bodies);
+}
+
+
+__global__ void triangle(int n0, int n1, float4 *bodies)
+{
+  int dn = n1 - n0;
+  if(dn > 1){
+    int nm = n0 + dn/2;
+
+    triangle <<< 1, 1 >>> (n0, n1, bodies);  //spawn new thread
+    triangle <<< 1, 1 >>>(nm,n1,bodies);  //we might try creating a __device__ version so we dont spawn this one, and instead keep working on this thread
+    rect <<< 1, 1 >>> (n0, nm, nm, n1, bodies);//spawn new thread
+  }
+}
+
 // rectangle kernel function ( without coarsening )
 
-//calculate_force: body x body interaction (from the gems3 algorithm)
 
-__device__ float3 
-bodyBodyInteraction(float3 ai, float4 bi, float4 bj) {
+__global__ void rect(int i0, int i1, int j0, int j1, float4 *bodies)
+{
+  int di = i1 - i0;
+  int dj = j1 -j0;
+
+  if(di > 1 && dj >1){
+    int im = i0 + di/2;
+    int jm = j0 + dj/2;
+
+    rect <<< 1, 1 >>>(i0, im, j0, jm, bodies); //spawn new threads
+    rect <<< 1, 1 >>>(im, i1, jm, j1, bodies); //we might try creating a __device__ version so we dont spawn this one, and instead keep working on this thread
+
+    rect <<< 1, 1 >>>(i0, im, jm, j1, bodies); //spawn new threads
+    rect <<< 1, 1 >>>(im, i1, j0, jm, bodies); //spawn new threads
+    
+  } else {
+    if (di > 0 && dj >0){
+      float3 resultingForce;
+      resultingForce = calculate_force(resultingForce, bodies[i0], bodies[j0]);
+      //add_force(&bodies[di],resultingForce);
+      bodies[di].x += resultingForce.x;
+      bodies[di].y += resultingForce.y;
+      bodies[di].z += resultingForce.z;
+      
+      //add_force(&bodies[dj], -resultingForce);
+      bodies[dj].x += -resultingForce.x;
+      bodies[dj].y += -resultingForce.y;
+      bodies[dj].z += -resultingForce.z;
+    }
+  }
+}
+
+
+//calculate_force: body x body interaction (from the gems3 algorithm : bodyBodyInteraction kernel function)
+
+__device__ float3 calculate_force(float3 ai, float4 bi, float4 bj) {
   float3 r;
 
   // r_ij  [3 FLOPS]
--- src/nbody_kernel.cuh
+++ src/nbody_kernel.cuh
@@ -2,7 +2,14 @@
 #define __NBODYKERNEL_CUH__
 
 
-__device__ float3 bodyBodyInteraction(float3 ai, float4 bi, float4 bj);
+
+__global__ void calculate_forces( int nbodies, float4 *bodies);
+
+__global__ void triangle(int n0, int n1, float4 *bodies);
+
+__global__ void rect(int i0, int i1, int j0, int j1, float4 *bodies);
+
+__device__ float3 calculate_force(float3 ai, float4 bi, float4 bj);
 
 __device__ void add_force(float4 *body, float3 force);
 
